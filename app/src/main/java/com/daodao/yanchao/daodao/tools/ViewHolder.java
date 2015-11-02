dbf984d5a05b4362669a63efd7254de584e798c8
yanchao <gelandesprung@163.com>
2015-11-02 14:42:28 +0800

    增加几个工具类，有连接状态的，时间类的还有一个ViewHolder的实现

---
+++ app/src/main/java/com/daodao/yanchao/daodao/tools/ViewHolder.java
@@ -0,0 +1,23 @@
+package com.daodao.yanchao.daodao.tools;
+
+import android.util.SparseArray;
+import android.view.View;
+
+/**
+ * Created by yanchao on 10/26/15.
+ */
+public class ViewHolder {
+    public static <T extends View> T get(View view, int id) {
+        SparseArray<View> viewHolder = (SparseArray<View>) view.getTag();
+        if (viewHolder == null) {
+            viewHolder = new SparseArray<View>();
+            view.setTag(viewHolder);
+        }
+        View childView = viewHolder.get(id);
+        if (childView == null) {
+            childView = view.findViewById(id);
+            viewHolder.put(id, childView);
+        }
+        return (T) childView;
+    }
+}
